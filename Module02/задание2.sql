13dd5b98660898b4b99a714cd5665b625ba200df
ElenaMoshenina <emoschenina@gmail.com>
2025-03-16 10:55:23 +0300

    Delete Module02 directory

--- Module02/задание2.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW dw.calendar_view AS --представление
-SELECT DISTINCT --устранение дубликатов, так как может быть множество заказов у одного клиента
-    --выбираем идентификатор, имя, сегмент, географические данные клиента
-    Order_date,
-	Ship_date
-FROM stg.orders;
-
-SELECT * FROM dw.calendar_view;
